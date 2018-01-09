c821e78352b57331a8bfba20749f71beae3cd9b9
takumi314 <isospin_symmetry@me.com>
2018-01-09 23:00:44 +0900

    set up layouts on Storyboard

--- Sample-Timer/ViewController.swift
+++ Sample-Timer/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var displayLabel: UILabel!
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func stepperOnTapped(_ sender: UIStepper) {
+    }
 
+    @IBAction func onStarted(_ sender: UIButton) {
+    }
+
+    @IBAction func onStopped(_ sender: UIButton) {
+    }
 }
 
