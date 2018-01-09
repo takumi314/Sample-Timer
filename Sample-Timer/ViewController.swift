3eaecec10613e8f4b421a51cbb79a75a1e7b9ee1
takumi314 <isospin_symmetry@me.com>
2018-01-09 22:46:16 +0900

    first commit

---
+++ Sample-Timer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sample-Timer
+//
+//  Created by NishiokaKohei on 2018/01/09.
+//  Copyright © 2018年 Kohey.Nishioka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
