79f4c6e70da0fa8159b506a94e867fc6fed668c1
takumi314 <isospin_symmetry@me.com>
2018-01-12 01:27:43 +0900

    add a value object

---
+++ Sample-Timer/ValueObjects/CountValue.swift
@@ -0,0 +1,13 @@
+//
+//  CountValue.swift
+//  Sample-Timer
+//
+//  Created by NishiokaKohei on 2018/01/12.
+//  Copyright © 2018年 Kohey.Nishioka. All rights reserved.
+//
+
+import Foundation
+
+struct CountValue {
+    
+}
